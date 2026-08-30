18eab577ca3b79ca9234a3bd2a241349b63d53e5
NaveeraRajChuhan <nnfstars@gmail.com>
2026-08-30 16:26:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from './CreateTicket.jsx'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+    <h1>Auth app</h1>
+
+    </div>
+      return <Dashboard />; 
+    </>
+  )
+}
+
+// naveera code
+
+import Dashboard from "./CreateTicket.jsx";
+
+
+
+
+// naveera code end
+
+export default App
