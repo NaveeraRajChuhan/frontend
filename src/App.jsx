5667665415f5c85d06d61dce473be55f5e119bc0
NaveeraRajChuhan <nnfstars@gmail.com>
2026-08-31 11:19:54 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Dashboard from './CreateTicket.jsx'
 
 function App() {
 
@@ -10,18 +9,8 @@ function App() {
     <h1>Auth app</h1>
 
     </div>
-      return <Dashboard />; 
     </>
   )
 }
-
-// naveera code
-
-import Dashboard from "./CreateTicket.jsx";
-
-
-
-
-// naveera code end
 
 export default App
