e67d5e50d4bd003d6ec998f2cb6287293bb08cf8
NaveeraRajChuhan <nnfstars@gmail.com>
2026-08-31 20:09:16 +0500

    added page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
             const response = await fetch(url,{
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json" 
+                    "Content-Type": "application/json"  
                 },
                 body: JSON.stringify(signupInfo) 
             });
