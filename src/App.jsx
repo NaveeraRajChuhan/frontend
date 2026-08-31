19e115ba48221d0baed4f1552643193b1e6a7527
NaveeraRajChuhan <nnfstars@gmail.com>
2026-08-31 14:00:52 +0500

    login etc pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import {Route, Routes, Navigate} from "react-router-dom";
+import './App.css';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 import Dashboard from "./dashboard";
 
 function App() {
@@ -7,10 +10,17 @@ function App() {
   return (
     <>
     <div className='App'>
-    <h1>Auth app</h1>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
+    </Routes>
 
     </div>
-      return <Dashboard />;
+       {/* <Dashboard />; */}
     </>
   )
 }
