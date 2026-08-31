535098dbe56e42e86294b4a2578467e93e804294
NaveeraRajChuhan <nnfstars@gmail.com>
2026-08-31 19:15:34 +0500

    signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import {ToastContainer} from 'react-toastify';
 
 const Signup = () => {
+
+    const [signupInfo, setSignupInfo] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+
+    const handleChange = (e) =>{
+        const { name, value } = e.target;
+        console.log(name, value);
+        const copySignupInfo = { ...signupInfo };
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    
+    }
+
+    const handleSignup = (e) =>{
+        e.preventDefault();
+    }
   return (
-    <div className= 'container'>
+    <div className= 'container'> 
       <h1>Signup</h1>
-      <form>
+      <form onSubmit={handleSignup}>
         <div>
             <label htmlFor="name">Name</label>
             <input
+            onChange={handleChange}
               type="text"
               name="name"
               autoFocus
@@ -19,6 +40,7 @@ const Signup = () => {
         <div>
             <label htmlFor="email">Email</label>
             <input
+             onChange={handleChange}
               type="email"
               name="email"
               placeholder="Enter your email...."
@@ -27,12 +49,13 @@ const Signup = () => {
         <div>
             <label htmlFor="password">Password</label>
             <input
+             onChange={handleChange}
               type="password"
               name="password"
               placeholder="Enter your password...."
             />
         </div>
-        <button>Signup</button>
+        <button type='submit'>Signup</button>
         <span>Already have an account ?
             <Link to="/login">Login</Link>
         </span>
